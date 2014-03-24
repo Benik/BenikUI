bb0dfc03881075c333f683d72fe169fe51dba898
Benik <benik@otenet.gr>
2014-03-25 01:08:08 +0200

    0.38beta
    *Done with skin styling. Not with this result without Azilroka. Big
    thanks :)
    *Added BenikUI Style in TinyDPS skin

--- modules/skins/skins.lua
+++ modules/skins/skins.lua
@@ -1,6 +1,7 @@
 local E, L, V, P, G, _ = unpack(ElvUI); --Import: Engine, Locales, PrivateDB, ProfileDB, GlobalDB, Localize Underscore
 local BUIS = E:NewModule('BuiSkins', "AceHook-3.0", 'AceEvent-3.0');
 local BUI = E:GetModule('BenikUI');
+local AS = E:GetModule('AddOnSkins', true)
 
 local SPACING = (E.PixelMode and 1 or 5)
 
@@ -16,7 +17,6 @@ local FreeBlizzFrames = {
 	ElvUI_StaticPopup1,
 	ElvUI_StaticPopup2,
 	ElvUI_StaticPopup3,
-	ElvUITutorialWindow, -- nope
 	FriendsFrame,
 	GameMenuFrame,
 	GossipFrame,
@@ -98,15 +98,21 @@ local BlizzUiFrames = {
 	{"Blizzard_VoidStorageUI", "VoidStorageFrame", "voidstorage"},
 }
 
+function BUI:StyleBlizzard(parent, ...)
+	local frame = CreateFrame('Frame', parent..'Decor', E.UIParent)
+	frame:CreateBackdrop('Default', true)
+	frame:SetParent(parent)
+	frame:Point('TOPLEFT', parent, 'TOPLEFT', SPACING, 5)
+	frame:Point('BOTTOMRIGHT', parent, 'TOPRIGHT', -SPACING, 0)
+end
+
 function BUIS:BlizzardUI_LOD_Skins(event, addon)
 	for i, v in ipairs(BlizzUiFrames) do
 		local blizzAddon, blizzFrame, elvoption = unpack( v )
-		--if (event == "ADDON_LOADED" and addon == blizzAddon) or (blizzFrame and not blizzFrame.style) then
-		if (event == "ADDON_LOADED" and addon == blizzAddon) then--or (blizzFrame and not blizzFrame.style) then
+		if (event == "ADDON_LOADED" and addon == blizzAddon) then
 			if E.private.skins.blizzard.enable ~= true or E.private.skins.blizzard[elvoption] ~= true then return end
 			if blizzFrame then
-				--blizzFrame:Style('Skin') -- Style them all
-				BUI:StyleBlizSkins(nil, blizzFrame)
+				BUI:StyleBlizzard(blizzFrame)
 				-- Fixes
 				if addon == "Blizzard_AchievementUI" then
 					if AchievementFrameCloseButton then
@@ -130,7 +136,7 @@ function BUIS:BlizzardUI_LOD_Skins(event, addon)
 					end
 				end
 				if addon == "Blizzard_GuildUI" then
-					GuildMemberDetailFrame:Style('Outside')
+					GuildMemberDetailFrame:Style('Skin')
 				end
 			end
 		end
@@ -163,98 +169,45 @@ function BUIS:BenikUISkins()
 	end)
 
 	-- Style Changes
-	DressUpFrame.style:Point('TOPLEFT', DressUpFrame, 'TOPLEFT', 7, 5)
-	DressUpFrame.style:Point('BOTTOMRIGHT', DressUpFrame, 'TOPRIGHT', -33, 0)
+	DressUpFrame.style:Point('TOPLEFT', DressUpFrame, 'TOPLEFT', 6, 5)
+	DressUpFrame.style:Point('BOTTOMRIGHT', DressUpFrame, 'TOPRIGHT', -32, -1)
 
-	MerchantFrame.style:Point('TOPLEFT', MerchantFrame, 'TOPLEFT', 7, 7)
-	MerchantFrame.style:Point('BOTTOMRIGHT', MerchantFrame, 'TOPRIGHT', SPACING, 2)
-
-	-- AddOn Styles
-	if IsAddOnLoaded('ElvUI_LocLite') then
-		local framestoskin = {LocationLitePanel, XCoordsLite, YCoordsLite}
-		for _, frame in pairs(framestoskin) do
-			frame:Style('Outside')
-		end
-	end
-	if IsAddOnLoaded('ElvUI_LocPlus') then
-		local framestoskin = {LeftCoordDtPanel, RightCoordDtPanel, LocationPlusPanel, XCoordsPanel, YCoordsPanel}
-		for _, frame in pairs(framestoskin) do
-			frame:Style('Outside')
-		end
-	end
+	MerchantFrame.style:Point('TOPLEFT', MerchantFrame, 'TOPLEFT', 6, 5)
+	MerchantFrame.style:Point('BOTTOMRIGHT', MerchantFrame, 'TOPRIGHT', 2, -1)
 end
 
 function BUIS:Initialize()
-	local AS = E:GetModule('AddOnSkins', true)
-	--if not AS then  -- Integrate into AddOnSkins otherwise run the skin changes.
-		--self:RegisterEvent('ADDON_LOADED', 'BlizzardUI_LOD_Skins')
-		--self:RegisterEvent('PLAYER_ENTERING_WORLD', 'BenikUISkins')
-		--return
-	--end
+	self:RegisterEvent('ADDON_LOADED', 'BlizzardUI_LOD_Skins')
+	self:RegisterEvent('PLAYER_ENTERING_WORLD', 'BenikUISkins')
+end
 
-	--[[
-	-- If you want the Decor on always then you will have to uncomment and adjust the resize function for the embed otherwise just delete this comment section.
-	function AS:EmbedSystem_WindowResize()
-		if InCombatLockdown() then return end
-		local DataTextSize = AS:CheckOption('EmbedLeftChat') and E.db.datatexts.leftChatPanel and LeftChatDataPanel:GetHeight() or E.db.datatexts.rightChatPanel and RightChatDataPanel:GetHeight() or 0
-		local ChatTabSize = AS:CheckOption('EmbedBelowTop') and RightChatTab:GetHeight() or 0
-		local Width = E.PixelMode and 6 or 10
-		local Height = E.PixelMode and 2 or 4
-		local Spacing = E.PixelMode and 6 or 8
-		local Total = AS.SLE and (Spacing + ChatTabSize + (E.PixelMode and 3 or 5)) or ((E.PixelMode and 11 or 16) + ChatTabSize + DataTextSize)
+----------------
+-- Style Recount
+----------------
+local function StyleRecount(name, parent, ...)
+	local frame = CreateFrame('Frame', name, E.UIParent)
+	frame:SetTemplate('Default', true)
+	frame:SetParent(parent)
+	frame:Point('TOPLEFT', parent, 'TOPLEFT', 0, -SPACING)
+	frame:Point('BOTTOMRIGHT', parent, 'TOPRIGHT', 0, -7)
+end
 
-		local ChatPanel = AS:CheckOption('EmbedLeftChat') and LeftChatPanel or RightChatPanel
-
-		EmbedSystem_MainWindow:SetParent(ChatPanel)
-
-		EmbedSystem_MainWindow:SetSize(ChatPanel:GetWidth() - Width, ChatPanel:GetHeight() - Total)
-		EmbedSystem_LeftWindow:SetSize(AS:CheckOption('EmbedLeftWidth'), EmbedSystem_MainWindow:GetHeight())
-		EmbedSystem_RightWindow:SetSize((EmbedSystem_MainWindow:GetWidth() - AS:CheckOption('EmbedLeftWidth')) - 1, EmbedSystem_MainWindow:GetHeight())
-
-		EmbedSystem_LeftWindow:SetPoint('LEFT', EmbedSystem_MainWindow, 'LEFT', 0, 0)
-		EmbedSystem_RightWindow:SetPoint('RIGHT', EmbedSystem_MainWindow, 'RIGHT', 0, 0)
-		EmbedSystem_MainWindow:SetPoint('BOTTOM', ChatPanel, 'BOTTOM', 0, (AS.SLE and (Spacing - 1) or (Spacing + DataTextSize)))
-
-		-- Dynamic Range
-		if IsAddOnLoaded('ElvUI_Config') then
-			E.Options.args.addonskins.args.embed.args.EmbedLeftWidth.min = floor(EmbedSystem_MainWindow:GetWidth() * .25)
-			E.Options.args.addonskins.args.embed.args.EmbedLeftWidth.max = floor(EmbedSystem_MainWindow:GetWidth() * .75)
-		end
-	end]]
-	
-
-	local function SkadaDecor()
-		if not AS:CheckAddOn('Skada') then return end
-		hooksecurefunc(Skada.displays['bar'], 'ApplySettings', function(self, win)
-			local skada = win.bargroup
-			skada.backdrop:Style('Outside', 'skadaDecor')
-			if win.db.enabletitle then
-				skada.button:StripTextures()
-			end
-		end)
-		hooksecurefunc(AS, 'Embed_Check', function(self, message)
-			if E.private.addonskins.EmbedSystem and E.private.addonskins.EmbedSkada then
-				skadaDecor:Hide()
-			else
-				skadaDecor:Show()
-			end
-		end)
-	end
-
-	local function RecountDecor()
-		if not AS:CheckAddOn('Recount') then return end
+local recountFrame = CreateFrame("Frame")
+recountFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+recountFrame:SetScript("OnEvent",function(self, event)
+	if IsAddOnLoaded("Recount") and IsAddOnLoaded("AddOnSkins_ElvUI") then
 		Recount_MainWindow.TitleBackground:StripTextures()
 		Recount_ConfigWindow.TitleBackground:StripTextures()
-		Recount_ConfigWindow:Style('Outside')
+		StyleRecount(nil, Recount_ConfigWindow)
 		hooksecurefunc(Recount, 'ShowReport', function(self)
-			Recount_ReportWindow.TitleBackground:StripTextures()
-			Recount_ReportWindow:Style('Outside')
+			if Recount_ReportWindow.TitleBackground then -- Without this check, it pops lua error
+				Recount_ReportWindow.TitleBackground:StripTextures()
+				StyleRecount(nil, Recount_ReportWindow)
+			end
 		end)
 		Recount_DetailWindow.TitleBackground:StripTextures()
-		Recount_DetailWindow:Style('Outside')
-		Recount_MainWindow:Style('Outside', 'RecountDecor')
-		RecountDecor:Point('TOPLEFT', Recount_MainWindow, 'TOPLEFT', 0, -SPACING)
-		RecountDecor:Point('BOTTOMRIGHT', parent, 'TOPRIGHT', 0, -7)
+		StyleRecount(nil, Recount_DetailWindow)
+		StyleRecount('RecountDecor', Recount_MainWindow)
 		hooksecurefunc(AS, 'Embed_Check', function(self, message)
 			if E.private.addonskins.EmbedSystem and E.private.addonskins.EmbedRecount then
 				RecountDecor:Hide()
@@ -263,19 +216,73 @@ function BUIS:Initialize()
 			end
 		end)
 	end
+	recountFrame:UnregisterEvent("PLAYER_ENTERING_WORLD")
+end)
 
-	local function BenikUISkins(event, addon)
-		if event == 'ADDON_LOADED' then
-			BUIS:BlizzardUI_LOD_Skins(event, addon)
-		else
-			BUIS:BenikUISkins()
+--------------
+-- Style Skada
+--------------
+local skadaFrame = CreateFrame("Frame")
+skadaFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+skadaFrame:SetScript("OnEvent",function(self, event)
+	if IsAddOnLoaded("Skada") and IsAddOnLoaded("AddOnSkins_ElvUI") then
+		local SkadaDisplayBar = Skada.displays['bar']
+		hooksecurefunc(SkadaDisplayBar, 'ApplySettings', function(self, win)
+			local skada = win.bargroup
+			skada.backdrop:Style('Outside')
+			if win.db.enabletitle then
+				skada.button:StripTextures()
+			end
+			if E.private.addonskins.EmbedSystem and E.private.addonskins.EmbedSkada then
+				skada.backdrop.style:Hide()
+			else
+				skada.backdrop.style:Show()
+			end
+		end)		
+	end
+	skadaFrame:UnregisterEvent("PLAYER_ENTERING_WORLD")
+end)
+
+----------------
+-- style TinyDPS
+----------------
+local tdFrame = CreateFrame("Frame")
+tdFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+tdFrame:SetScript("OnEvent",function(self, event)
+	if IsAddOnLoaded("TinyDPS") and IsAddOnLoaded("AddOnSkins_ElvUI") then
+		tdpsFrame:Style('Outside')
+	end
+	tdFrame:UnregisterEvent("PLAYER_ENTERING_WORLD")
+end)
+
+---------------
+-- LocationPlus
+---------------
+local lpFrame = CreateFrame("Frame")
+lpFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+lpFrame:SetScript("OnEvent",function(self, event)
+	if IsAddOnLoaded("ElvUI_LocPlus") then
+		local framestoskin = {LeftCoordDtPanel, RightCoordDtPanel, LocationPlusPanel, XCoordsPanel, YCoordsPanel}
+		for _, frame in pairs(framestoskin) do
+			frame:Style('Outside')
 		end
 	end
-	self:RegisterEvent('ADDON_LOADED', 'BlizzardUI_LOD_Skins')
-	self:RegisterEvent('PLAYER_ENTERING_WORLD', 'BenikUISkins')
-	AS:RegisterSkin('BenikUI', BenikUISkins, 'ADDON_LOADED')
-	AS:RegisterSkin('SkadaSkin', SkadaDecor, 2) -- Priority 2 will run after my skin.
-	AS:RegisterSkin('RecountSkin', RecountDecor, 2) -- Priority 2 will run after my skin.
-end
+	lpFrame:UnregisterEvent("PLAYER_ENTERING_WORLD")
+end)
+
+---------------
+-- LocationLite
+---------------
+local llFrame = CreateFrame("Frame")
+llFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+llFrame:SetScript("OnEvent",function(self, event)
+	if IsAddOnLoaded("ElvUI_LocLite") then
+		local framestoskin = {LocationLitePanel, XCoordsLite, YCoordsLite}
+		for _, frame in pairs(framestoskin) do
+			frame:Style('Outside')
+		end
+	end
+	llFrame:UnregisterEvent("PLAYER_ENTERING_WORLD")
+end)
 
 E:RegisterModule(BUIS:GetName())
