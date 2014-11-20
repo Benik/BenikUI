0b46fc8289b8796c92008d60efe378767ba64068
Benik <benik@otenet.gr>
2014-11-20 12:13:32 +0200

    Versatility Datatext

---
+++ ElvUI_BenikUI/modules/datatexts/versatility.lua
@@ -0,0 +1,36 @@
+local E, L, V, P, G = unpack(ElvUI); --Inport: Engine, Locales, PrivateDB, ProfileDB, GlobalDB
+local DT = E:GetModule('DataTexts')
+
+local lastPanel;
+local displayNumberString = ''
+local join = string.join
+
+local function OnEvent(self, event, unit)
+	local versatilityDamageBonus = GetCombatRatingBonus(29) + GetVersatilityBonus(29)
+
+	self.text:SetFormattedText(displayNumberString, STAT_VERSATILITY, versatilityDamageBonus)
+
+	lastPanel = self
+end
+
+local function ValueColorUpdate(hex, r, g, b)
+	displayNumberString = join("", "%s: ", hex, "%.2f%%|r")
+	
+	if lastPanel ~= nil then
+		OnEvent(lastPanel)
+	end
+end
+E['valueColorUpdateFuncs'][ValueColorUpdate] = true
+
+--[[
+	DT:RegisterDatatext(name, events, eventFunc, updateFunc, clickFunc, onEnterFunc, onLeaveFunc)
+	
+	name - name of the datatext (required)
+	events - must be a table with string values of event names to register 
+	eventFunc - function that gets fired when an event gets triggered
+	updateFunc - onUpdate script target function
+	click - function to fire when clicking the datatext
+	onEnterFunc - function to fire OnEnter
+	onLeaveFunc - function to fire OnLeave, if not provided one will be set for you that hides the tooltip.
+]]
+DT:RegisterDatatext('BuiVersatility', {"UNIT_STATS", "UNIT_AURA", "ACTIVE_TALENT_GROUP_CHANGED", "PLAYER_TALENT_UPDATE"}, OnEvent)
