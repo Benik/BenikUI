a28fe45d79dd9832a3f709ff02323f0967225a62
Benik <benik@otenet.gr>
2015-10-10 01:13:26 +0300

    Added a new spec switch datatext that only shows current spec. LeftClick to re-spec RightClick for info and loot spec (as in ElvUI)

---
+++ ElvUI_BenikUI/modules/datatexts/specswitch.lua
@@ -0,0 +1,116 @@
+local E, L, V, P, G, _ = unpack(ElvUI); --Import: Engine, Locales, PrivateDB, ProfileDB, GlobalDB, Localize Underscore
+local DT = E:GetModule('DataTexts')
+
+local format = string.format
+local join = string.join
+local lastPanel, active
+local displayString = '';
+local talent = {}
+local activeString = join("", "|cff00FF00" , ACTIVE_PETS, "|r")
+local inactiveString = join("", "|cffFF0000", FACTION_INACTIVE, "|r")
+
+local menuFrame = CreateFrame("Frame", "LootSpecializationDatatextClickMenu", E.UIParent, "UIDropDownMenuTemplate")
+menuFrame:SetTemplate('Transparent', true)
+
+local menuList = {
+	{ text = SELECT_LOOT_SPECIALIZATION, isTitle = true, notCheckable = true },
+	{ notCheckable = true, func = function() SetLootSpecialization(0) end },
+	{ notCheckable = true },
+	{ notCheckable = true },
+	{ notCheckable = true },
+	{ notCheckable = true }
+}
+
+
+local function OnEvent(self, event)
+	lastPanel = self
+
+	local specIndex = GetSpecialization();
+	if not specIndex then return end
+
+	active = GetActiveSpecGroup()
+
+	local talent = ''
+	if GetSpecialization(false, false, active) then
+		talent = format('%s', select(2, GetSpecializationInfo(GetSpecialization(false, false, active))))
+	end
+
+	self.text:SetFormattedText('%s', talent)
+end
+
+local function OnEnter(self)
+	DT:SetupTooltip(self)
+
+	for i = 1, GetNumSpecGroups() do
+		if GetSpecialization(false, false, i) then
+			DT.tooltip:AddLine(join(" ", format(displayString, select(2, GetSpecializationInfo(GetSpecialization(false, false, i)))), (i == active and activeString or inactiveString)),1,1,1)
+		end
+	end
+
+	DT.tooltip:AddLine(' ')
+	local specialization = GetLootSpecialization()
+	if specialization == 0 then
+		local specIndex = GetSpecialization();
+
+		if specIndex then
+			local specID, name = GetSpecializationInfo(specIndex);
+			DT.tooltip:AddLine(format('|cffFFFFFF%s:|r %s', SELECT_LOOT_SPECIALIZATION, format(LOOT_SPECIALIZATION_DEFAULT, name)))
+		end
+	else
+		local specID, name = GetSpecializationInfoByID(specialization);
+		if specID then
+			DT.tooltip:AddLine(format('|cffFFFFFF%s:|r %s', SELECT_LOOT_SPECIALIZATION, name))
+		end
+	end
+
+	DT.tooltip:AddLine(' ')
+	DT.tooltip:AddLine(L["|cffFFFFFFLeft Click:|r Change Talent Specialization"])
+	DT.tooltip:AddLine(L["|cffFFFFFFRight Click:|r Change Loot Specialization"])
+
+	DT.tooltip:Show()
+end
+
+local function OnClick(self, button)
+	local specIndex = GetSpecialization();
+	if not specIndex then return end
+
+	if button == "LeftButton" then
+		SetActiveSpecGroup(active == 1 and 2 or 1)
+	else
+		DT.tooltip:Hide()
+		local specID, specName = GetSpecializationInfo(specIndex);
+		menuList[2].text = format(LOOT_SPECIALIZATION_DEFAULT, specName);
+
+		for index = 1, 4 do
+			local id, name = GetSpecializationInfo(index);
+			if ( id ) then
+				menuList[index + 2].text = name
+				menuList[index + 2].func = function() SetLootSpecialization(id) end
+			else
+				menuList[index + 2] = nil
+			end
+		end
+		EasyMenu(menuList, menuFrame, "cursor", -15, -7, "MENU", 2)
+	end
+end
+
+local function ValueColorUpdate(hex, r, g, b)
+	displayString = join("", "|cffFFFFFF%s:|r ")
+
+	if lastPanel ~= nil then
+		OnEvent(lastPanel)
+	end
+end
+E['valueColorUpdateFuncs'][ValueColorUpdate] = true
+
+--[[
+	DT:RegisterDatatext(name, events, eventFunc, updateFunc, clickFunc, onEnterFunc)
+
+	name - name of the datatext (required)
+	events - must be a table with string values of event names to register
+	eventFunc - function that gets fired when an event gets triggered
+	updateFunc - onUpdate script target function
+	click - function to fire when clicking the datatext
+	onEnterFunc - function to fire OnEnter
+]]
+DT:RegisterDatatext('Spec Switch (BenikUI)',{"PLAYER_ENTERING_WORLD", "CHARACTER_POINTS_CHANGED", "PLAYER_TALENT_UPDATE", "ACTIVE_TALENT_GROUP_CHANGED"}, OnEvent, nil, OnClick, OnEnter)
