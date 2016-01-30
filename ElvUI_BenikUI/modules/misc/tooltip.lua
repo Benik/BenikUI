87131c7044d03aa330d88e6bee6738e33c20d2c1
Benik <benik@otenet.gr>
2016-01-30 22:40:53 +0200

    Sometimes the easiest way is the one you will never think of

--- ElvUI_BenikUI/modules/misc/tooltip.lua
+++ ElvUI_BenikUI/modules/misc/tooltip.lua
@@ -1,14 +1,20 @@
 local E, L, V, P, G = unpack(ElvUI);
 local BTT = E:NewModule('BenikUI_Tooltip');
-local BUI = E:GetModule('BenikUI');
 local TT = E:GetModule('Tooltip')
 
 local GameTooltip, GameTooltipStatusBar = _G["GameTooltip"], _G["GameTooltipStatusBar"]
 local IsAddOnLoaded = IsAddOnLoaded
 
+local ttr, ttg, ttb = 0, 0, 0
+
 local function StyleTooltip()
 	GameTooltip:Style('Outside')
-
+	
+	-- Grab the style color
+	local r, g, b = GameTooltip.style.color:GetVertexColor()
+	ttr, ttg, ttb = r, g, b
+	
+	-- FreebTip support
 	if IsAddOnLoaded('FreebTip') then
 		GameTooltip.style:ClearAllPoints()
 		GameTooltip.style:Point('TOPLEFT', GameTooltip, 'TOPLEFT', (E.PixelMode and 1 or 0), (E.PixelMode and -1 or 7))
@@ -16,15 +22,27 @@ local function StyleTooltip()
 	end
 end
 
-local function RecolorTooltipStyle(tt)
-	local r, g, b = GameTooltipStatusBar:GetStatusBarColor()
+local function RecolorTooltipStyle()
+	local r, g, b = 0, 0, 0
+
+	if GameTooltipStatusBar:IsShown() then
+		r, g, b = GameTooltipStatusBar:GetStatusBarColor()	
+	else
+		r, g, b = ttr, ttg, ttb
+	end
 	GameTooltip.style.color:SetVertexColor(r, g, b)
 end
 
 function BTT:Initialize()
 	if E.db.bui.buiStyle ~= true then return end
 	StyleTooltip()
+	hooksecurefunc(TT, "SetUnitAura", RecolorTooltipStyle)
+	hooksecurefunc(TT, "SetConsolidatedUnitAura", RecolorTooltipStyle)
+	hooksecurefunc(TT, "GameTooltip_OnTooltipSetSpell", RecolorTooltipStyle)
+	hooksecurefunc(TT, "GameTooltip_OnTooltipCleared", RecolorTooltipStyle)
+	hooksecurefunc(TT, "GameTooltip_OnTooltipSetItem", RecolorTooltipStyle)
 	hooksecurefunc(TT, "GameTooltip_OnTooltipSetUnit", RecolorTooltipStyle)
+	hooksecurefunc(TT, "GameTooltipStatusBar_OnValueChanged", RecolorTooltipStyle)
 end
 
 E:RegisterModule(BTT:GetName())
