9ccc59c1f1f579c17088950eaefa01960aaee503
Benik <benik@otenet.gr>
2016-01-31 02:17:08 +0200

    Holy Merathilis. /bow

--- ElvUI_BenikUI/modules/unitframes/unitframes.lua
+++ ElvUI_BenikUI/modules/unitframes/unitframes.lua
@@ -1,5 +1,5 @@
 local E, L, V, P, G, _ = unpack(ElvUI);
-local UFB = E:NewModule('BuiUnits', 'AceHook-3.0', 'AceEvent-3.0');
+local UFB = E:NewModule('BuiUnits', 'AceHook-3.0', 'AceEvent-3.0', 'AceTimer-3.0');
 local UF = E:GetModule('UnitFrames');
 local LSM = LibStub("LibSharedMedia-3.0");
 UF.LSM = LSM
