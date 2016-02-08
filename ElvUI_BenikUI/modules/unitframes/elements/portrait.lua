e5996d20b4bf53a109b2952ee8e1e2f58ff965c2
Benik <benik@otenet.gr>
2016-02-08 22:00:53 +0200

    Created portrait edit for all frames except player/target

--- ElvUI_BenikUI/modules/unitframes/elements/portrait.lua
+++ ElvUI_BenikUI/modules/unitframes/elements/portrait.lua
@@ -2,6 +2,64 @@
 local UFB = E:GetModule('BuiUnits');
 local UF = E:GetModule('UnitFrames');
 
+function UFB:Configure_SimplePortrait(frame)
+	local portrait = frame.Portrait
+	local db = frame.db
+	
+	local portrait = frame.Portrait
+	if frame.USE_PORTRAIT then
+		if frame.USE_PORTRAIT_OVERLAY then
+			if db.portrait.style == '3D' then
+				portrait:SetFrameLevel(frame.Health:GetFrameLevel() + 1)
+			end
+
+			portrait:SetAllPoints(frame.Health)
+			portrait:SetAlpha(0.3)
+			portrait.backdrop:Hide()			
+		else		
+			portrait:SetAlpha(1)
+			portrait.backdrop:ClearAllPoints()
+			portrait.backdrop:Show()
+
+			if db.portrait.style == '3D' then
+				portrait:SetFrameLevel(frame.Health:GetFrameLevel() -4) --Make sure portrait is behind Health and Power
+			end
+			
+			if frame.ORIENTATION == "LEFT" then
+				portrait.backdrop:Point("TOPLEFT", frame, "TOPLEFT", frame.SPACING, frame.USE_MINI_CLASSBAR and -(frame.CLASSBAR_YOFFSET+frame.SPACING) or -frame.SPACING)
+				if frame.USE_EMPTY_BAR and not frame.USE_POWERBAR_OFFSET then
+					portrait.backdrop:Point("BOTTOMRIGHT", frame.EmptyBar, "BOTTOMLEFT", frame.BORDER - frame.SPACING*3, 0)						
+				else
+					if frame.USE_MINI_POWERBAR or frame.USE_POWERBAR_OFFSET or not frame.USE_POWERBAR or frame.USE_INSET_POWERBAR or frame.POWERBAR_DETACHED then
+						portrait.backdrop:Point("BOTTOMRIGHT", frame.Health.backdrop, "BOTTOMLEFT", frame.BORDER - frame.SPACING*3, 0)
+					else
+						portrait.backdrop:Point("BOTTOMRIGHT", frame.Power.backdrop, "BOTTOMLEFT", frame.BORDER - frame.SPACING*3, 0)
+					end
+				end
+			elseif frame.ORIENTATION == "RIGHT" then
+				portrait.backdrop:Point("TOPRIGHT", frame, "TOPRIGHT", -frame.SPACING, frame.USE_MINI_CLASSBAR and -(frame.CLASSBAR_YOFFSET+frame.SPACING) or -frame.SPACING)
+				if frame.USE_EMPTY_BAR and not frame.USE_POWERBAR_OFFSET then
+					portrait.backdrop:Point("BOTTOMLEFT", frame.EmptyBar, "BOTTOMRIGHT", -frame.BORDER + frame.SPACING*3, 0)						
+				else
+					if frame.USE_MINI_POWERBAR or frame.USE_POWERBAR_OFFSET or not frame.USE_POWERBAR or frame.USE_INSET_POWERBAR or frame.POWERBAR_DETACHED then
+						portrait.backdrop:Point("BOTTOMLEFT", frame.Health.backdrop, "BOTTOMRIGHT", -frame.BORDER + frame.SPACING*3, 0)
+					else
+						portrait.backdrop:Point("BOTTOMLEFT", frame.Power.backdrop, "BOTTOMRIGHT", -frame.BORDER + frame.SPACING*3, 0)
+					end
+				end
+			end	
+
+			-- ElvUI setting
+			portrait:SetInside(portrait.backdrop, frame.BORDER)
+			
+			-- Keeping BenikUI setting in case of impelementation
+			--portrait:ClearAllPoints()
+			--portrait:Point('BOTTOMLEFT', portrait.backdrop, 'BOTTOMLEFT', frame.BORDER, frame.BORDER)		
+			--portrait:Point('TOPRIGHT', portrait.backdrop, 'TOPRIGHT', -(E.PixelMode and db.portrait.style == '3D' and frame.BORDER*2 or frame.BORDER), -frame.BORDER)
+		end
+	end
+end
+
 function UFB:Configure_Portrait(frame, detached, isPlayer, isTarget)
 	local portrait = frame.Portrait
 	local db = frame.db
