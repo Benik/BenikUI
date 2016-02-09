f40a998a245bc476f22ff14e90972440201b7707
Benik <benik@otenet.gr>
2016-02-09 23:40:57 +0200

    Target Glow party frame fix

--- ElvUI_BenikUI/modules/unitframes/elements/targetglow.lua
+++ ElvUI_BenikUI/modules/unitframes/elements/targetglow.lua
@@ -6,9 +6,14 @@ function UFB:Configure_TargetGlow(frame)
 	local SHADOW_SPACING = frame.SHADOW_SPACING
 	local tGlow = frame.TargetGlow
 	tGlow:ClearAllPoints()
-
-	tGlow:Point("TOPLEFT", -SHADOW_SPACING, SHADOW_SPACING)
-	tGlow:Point("TOPRIGHT", SHADOW_SPACING, SHADOW_SPACING)
+	
+	if frame.PORTRAIT_HEIGHT and frame.PORTRAIT_HEIGHT > 0 then
+		tGlow:Point("TOPLEFT", frame.Health.backdrop, "TOPLEFT", -SHADOW_SPACING, SHADOW_SPACING)
+		tGlow:Point("TOPRIGHT", frame.Health.backdrop, "TOPRIGHT", SHADOW_SPACING, SHADOW_SPACING)
+	else
+		tGlow:Point("TOPLEFT", -SHADOW_SPACING, SHADOW_SPACING)
+		tGlow:Point("TOPRIGHT", SHADOW_SPACING, SHADOW_SPACING)
+	end
 
 	if frame.USE_MINI_POWERBAR then
 		if frame.USE_EMPTY_BAR then
