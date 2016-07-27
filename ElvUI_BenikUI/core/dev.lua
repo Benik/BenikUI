9b42b11d135e5c9231eb4d2d054710ef16a83158
benik <benik@otenet.gr>
2016-07-28 01:51:40 +0300

    test

--- ElvUI_BenikUI/core/dev.lua
+++ ElvUI_BenikUI/core/dev.lua
@@ -1,7 +1,25 @@
 ﻿local E, L, V, P, G = unpack(ElvUI);
 
---[[local test = CreateFrame("Frame", nil, E.UIparent)
-test:SetTemplate('Transparent')
-test:CreateSoftShadow()
-test:Size(400, 300)
-test:Point('CENTER')]]
+--	/run LootAlertSystem:AddAlert("|cff9d9d9d|Hitem:7073:0:0:0:0:0:0:0:80:0:0:0:0|h[Broken Fang]|h|r", 1, specID, 3, 1, 3, Awesome);
+     
+--	/run MoneyWonAlertSystem:AddAlert(815)
+     
+--	/run AchievementAlertSystem:AddAlert(5192)
+     
+--	/run CriteriaAlertSystem:ShowAlert(80,1)
+     
+--	/run GuildChallengeAlertSystem:AddAlert(3, 2, 5)
+     
+--	/run InvasionAlertSystem:AddAlert(1,20)
+     
+--	/run DigsiteCompleteAlertSystem:AddAlert(1)
+     
+--	/run LootUpgradeAlertSystem:AddAlert("|cff9d9d9d|Hitem:7073:0:0:0:0:0:0:0:80:0:0:0:0|h[Broken Fang]|h|r", 1, specID, 3, "Cesaor", 3, Awesome);
+     
+--	/run GarrisonFollowerAlertSystem:AddAlert(112, "Cool Guy", "100", 3, 1)
+     
+--	/run GarrisonShipFollowerAlertSystem:AddAlert(592, "Test", "Transport", "GarrBuilding_Barracks_1_H", 3, 2, 1)
+     
+--	/run GarrisonBuildingAlertSystem:AddAlert("miau")
+     
+--	/run WorldQuestCompleteAlertSystem:AddAlert(112)
