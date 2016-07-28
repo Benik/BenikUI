2199f5cca69531753871c3c6ca5a01a8eff3750e
benik <benik@otenet.gr>
2016-07-28 23:24:23 +0300

    nada

--- ElvUI_BenikUI/core/dev.lua
+++ ElvUI_BenikUI/core/dev.lua
@@ -1,25 +1,23 @@
 ﻿local E, L, V, P, G = unpack(ElvUI);
 
---	/run LootAlertSystem:AddAlert("|cff9d9d9d|Hitem:7073:0:0:0:0:0:0:0:80:0:0:0:0|h[Broken Fang]|h|r", 1, specID, 3, 1, 3, Awesome);
-     
---	/run MoneyWonAlertSystem:AddAlert(815)
-     
---	/run AchievementAlertSystem:AddAlert(5192)
-     
---	/run CriteriaAlertSystem:ShowAlert(80,1)
-     
---	/run GuildChallengeAlertSystem:AddAlert(3, 2, 5)
-     
---	/run InvasionAlertSystem:AddAlert(1,20)
-     
---	/run DigsiteCompleteAlertSystem:AddAlert(1)
-     
---	/run LootUpgradeAlertSystem:AddAlert("|cff9d9d9d|Hitem:7073:0:0:0:0:0:0:0:80:0:0:0:0|h[Broken Fang]|h|r", 1, specID, 3, "Cesaor", 3, Awesome);
-     
---	/run GarrisonFollowerAlertSystem:AddAlert(112, "Cool Guy", "100", 3, 1)
-     
---	/run GarrisonShipFollowerAlertSystem:AddAlert(592, "Test", "Transport", "GarrBuilding_Barracks_1_H", 3, 2, 1)
-     
---	/run GarrisonBuildingAlertSystem:AddAlert("miau")
-     
---	/run WorldQuestCompleteAlertSystem:AddAlert(112)
+	--[[ Code you can use for alert testing
+		--Queued Alerts:
+		/run AchievementAlertSystem:AddAlert(5192)
+		/run CriteriaAlertSystem:AddAlert(9023, "Doing great!")
+		/run LootAlertSystem:AddAlert("\124cffa335ee\124Hitem:18832::::::::::\124h[Brutality Blade]\124h\124r", 1, 1, 1, 1, false, false, 0, false, false)
+		/run LootUpgradeAlertSystem:AddAlert("\124cffa335ee\124Hitem:18832::::::::::\124h[Brutality Blade]\124h\124r", 1, 1, 1, nil, nil, false)
+		/run MoneyWonAlertSystem:AddAlert(815)
+		/run NewRecipeLearnedAlertSystem:AddAlert(204)
+		
+		--Simple Alerts
+		/run GuildChallengeAlertSystem:AddAlert(3, 2, 5)
+		/run InvasionAlertSystem:AddAlert(1)
+		/run WorldQuestCompleteAlertSystem:AddAlert(112)
+		/run GarrisonBuildingAlertSystem:AddAlert("Barracks")
+		/run GarrisonFollowerAlertSystem:AddAlert(204, "Ben Stone", 90, 3, false)
+		/run GarrisonMissionAlertSystem:AddAlert(681) (Requires a mission ID that is in your mission list.)
+		/run GarrisonShipFollowerAlertSystem:AddAlert(592, "Test", "Transport", "GarrBuilding_Barracks_1_H", 3, 2, 1)
+		/run LegendaryItemAlertSystem:AddAlert("\124cffa335ee\124Hitem:18832::::::::::\124h[Brutality Blade]\124h\124r")
+		/run StorePurchaseAlertSystem:AddAlert("\124cffa335ee\124Hitem:180545::::::::::\124h[Mystic Runesaber]\124h\124r", "", "", 214)
+		/run DigsiteCompleteAlertSystem:AddAlert(1)
+	]]
