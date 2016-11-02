c777cd898690d7850664ff70a7396c63e9992806
Benik <benik@otenet.gr>
2016-11-02 23:37:59 +0200

    a, b, c, d, e, etc :)

--- ElvUI_BenikUI/core/install.lua
+++ ElvUI_BenikUI/core/install.lua
@@ -1178,52 +1178,52 @@ local function SetupAddOnSkins()
 			end
 		else
 			-- reset the embeds to defaults, in case of Skada/Recount/Details swap
-			AS.db['EmbedSystem'] = false
-			AS.db['EmbedSystemDual'] = false
-			AS.db['EmbedBelowTop'] = false
-			AS.db['TransparentEmbed'] = false
-			AS.db['EmbedMain'] = 'Skada'
-			AS.db['EmbedLeft'] = 'Skada'
-			AS.db['EmbedRight'] = 'Skada'
-			AS.db['RecountBackdrop'] = true
-			AS.db['SkadaBackdrop'] = true
-			AS.db['DetailsBackdrop'] = true
-			AS.db['DBMSkinHalf'] = false
 			AS.db['DBMFont'] = 'Tukui'
 			AS.db['DBMFontSize'] = 12
 			AS.db['DBMRadarTrans'] = false
-			AS.db['Parchment'] = false
+			AS.db['DBMSkinHalf'] = false
+			AS.db['DetailsBackdrop'] = true
+			AS.db['EmbedBelowTop'] = false
 			AS.db['EmbedFrameStrata'] = "2-LOW"
+			AS.db['EmbedLeft'] = 'Skada'
+			AS.db['EmbedMain'] = 'Skada'
+			AS.db['EmbedRight'] = 'Skada'
+			AS.db['EmbedSystem'] = false
+			AS.db['EmbedSystemDual'] = false
+			AS.db['Parchment'] = false
+			AS.db['RecountBackdrop'] = true
+			AS.db['SkadaBackdrop'] = true
+			AS.db['TransparentEmbed'] = false
 			
 			if IsAddOnLoaded('Recount') then
+				AS.db['EmbedBelowTop'] = false
 				AS.db['EmbedMain'] = 'Recount'
 				AS.db['EmbedSystem'] = true
 				AS.db['EmbedSystemDual'] = false
 				AS.db['RecountBackdrop'] = false
-				AS.db['EmbedBelowTop'] = false
 				AS.db['TransparentEmbed'] = true
 			end
 			
 			if IsAddOnLoaded('Skada') then
+				AS.db['EmbedBelowTop'] = false
+				AS.db['EmbedLeft'] = 'Skada'
+				AS.db['EmbedMain'] = 'Skada'
+				AS.db['EmbedRight'] = 'Skada'
 				AS.db['EmbedSystem'] = false
 				AS.db['EmbedSystemDual'] = true
-				AS.db['EmbedBelowTop'] = false
-				AS.db['TransparentEmbed'] = true
 				AS.db['SkadaBackdrop'] = false
-				AS.db['EmbedMain'] = 'Skada'
-				AS.db['EmbedLeft'] = 'Skada'
-				AS.db['EmbedRight'] = 'Skada'
+				AS.db['TransparentEmbed'] = true
 			end
 			
 			if IsAddOnLoaded('Details') then
+				AS.db['DetailsBackdrop'] = false
+				AS.db['EmbedBelowTop'] = false
+				AS.db['EmbedLeft'] = 'Details'
+				AS.db['EmbedMain'] = 'Details'
+				AS.db['EmbedRight'] = 'Details'
 				AS.db['EmbedSystem'] = false
 				AS.db['EmbedSystemDual'] = true
-				AS.db['EmbedBelowTop'] = false
 				AS.db['TransparentEmbed'] = true
-				AS.db['DetailsBackdrop'] = false
-				AS.db['EmbedMain'] = 'Details'
-				AS.db['EmbedLeft'] = 'Details'
-				AS.db['EmbedRight'] = 'Details'
 			end
 			
 			if IsAddOnLoaded('DBM-Core') then
