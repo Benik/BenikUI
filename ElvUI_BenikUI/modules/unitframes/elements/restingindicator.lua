8b2f65cf79f6dd862f1077022812756572099940
Benik <benik@otenet.gr>
2016-12-27 19:47:30 +0200

    Minor fix for the Resting indicator

--- ElvUI_BenikUI/modules/unitframes/elements/restingindicator.lua
+++ ElvUI_BenikUI/modules/unitframes/elements/restingindicator.lua
@@ -10,10 +10,10 @@ function UFB:Configure_RestingIndicator(frame)
 		if frame.USE_PORTRAIT and not frame.USE_PORTRAIT_OVERLAY and frame.PORTRAIT_DETACHED then
 			if frame.PORTRAIT_STYLING and frame.Portrait.backdrop.style then
 				rIcon:SetParent(frame.Portrait.backdrop.style)
-				rIcon:Point("CENTER", frame.Portrait.backdrop.style, "TOPLEFT")					
+				rIcon:Point("CENTER", frame.Portrait.backdrop.style, "TOPLEFT", -3, 6)
 			else
 				rIcon:SetParent(frame)
-				rIcon:Point("CENTER", frame.Portrait, "TOPLEFT", -3, 7)
+				rIcon:Point("CENTER", frame.Health, "TOPLEFT", -3, 6)
 			end
 		else
 			rIcon:SetParent(frame)
