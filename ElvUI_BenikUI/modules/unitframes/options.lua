63d6b9657ce0a23393b73d388f874ca1d873fcc6
Merathilis <neo0608@gmail.com>
2017-03-17 16:49:14 +0100

    Halp... Try to disable the castbar options, if the infopanel are disabled.

--- ElvUI_BenikUI/modules/unitframes/options.lua
+++ ElvUI_BenikUI/modules/unitframes/options.lua
@@ -129,6 +129,7 @@ local function ufTable()
 				type = 'group',
 				name = L['Castbar'].." ("..PLAYER.."/"..TARGET..")",
 				guiInline = true,
+				-- disabled = function() return not E.db.unitframe.units[groupName]['infoPanel'] end, -- disable the yOffset if the InfoPanel are not enabled.
 				get = function(info) return E.db.benikui.unitframes.castbar.text[ info[#info] ] end,
 				set = function(info, value) E.db.benikui.unitframes.castbar.text[ info[#info] ] = value; BUIC:UpdateAllCastbars(); end,
 				args = {
