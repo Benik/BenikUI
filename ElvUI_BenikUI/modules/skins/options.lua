a4ae7e560e0dc7c54870d3ac9f0cb2bcde90fe75
Benik <benik@otenet.gr>
2017-09-14 17:33:18 +0300

    Disabled buttons on not loaded addons look much better. <3 Simpy

--- ElvUI_BenikUI/modules/skins/options.lua
+++ ElvUI_BenikUI/modules/skins/options.lua
@@ -157,6 +157,7 @@ local function SkinTable()
 			type = 'execute',
 			name = addonName,
 			desc = L['This will create and apply profile for ']..addonName,
+			buttonElvUI = true,
 			func = function()
 				if addon == 'DBM-Core' then
 					BUI:LoadDBMProfile()
