dc465a13c730a3485a0d14fe50c7873f182cbba0
Benik <benik@otenet.gr>
2017-10-27 22:51:43 +0300

    Just in case Styling is disabled

--- ElvUI_BenikUI/layout/layout.lua
+++ ElvUI_BenikUI/layout/layout.lua
@@ -540,7 +540,7 @@ local function InjectMinimapOption()
 		order = 3,
 		type = "toggle",
 		name = BUI:cOption(L['BenikUI Style']),
-		disabled = function() return not E.private.general.minimap.enable end,
+		disabled = function() return not E.private.general.minimap.enable or not E.db.benikui.general.benikuiStyle end,
 		get = function(info) return E.db.general.minimap.benikuiStyle end,
 		set = function(info, value) E.db.general.minimap.benikuiStyle = value; BUIL:ToggleMinimapStyle(); end,
 	}
