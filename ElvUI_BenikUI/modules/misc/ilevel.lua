395254d74a4be43a6335ab008eff1a9ac702a9c7
Benik <benik@otenet.gr>
2018-02-19 01:41:02 +0200

    Went back to tooltip scanning for the iLevel. Still needs some love.

--- ElvUI_BenikUI/modules/misc/ilevel.lua
+++ ElvUI_BenikUI/modules/misc/ilevel.lua
@@ -4,29 +4,73 @@ local LSM = LibStub('LibSharedMedia-3.0')
 -- Based on iLevel addon by ahak. http://www.curse.com/addons/wow/ilevel
 
 local _G = _G
+local match, gsub = string.match, gsub
 
 local CreateFrame = CreateFrame
+local SetInventoryItem = SetInventoryItem
 local GetInventoryItemLink = GetInventoryItemLink
 local GetItemInfo = GetItemInfo
 local GetItemQualityColor = GetItemQualityColor
-local GetDetailedItemLevelInfo = GetDetailedItemLevelInfo
 
 -- GLOBALS: CharacterHeadSlot, CharacterNeckSlot, CharacterShoulderSlot, CharacterBackSlot, CharacterChestSlot, CharacterWristSlot
 -- GLOBALS: CharacterHandsSlot, CharacterWaistSlot, CharacterLegsSlot, CharacterFeetSlot, CharacterFinger0Slot, CharacterFinger1Slot
 -- GLOBALS: CharacterTrinket0Slot, CharacterTrinket1Slot, CharacterMainHandSlot, CharacterSecondaryHandSlot, PaperDollFrame
 
-local xo, yo = 0, 1
+local equipped = {}
+
+local slotTable = {
+	"HeadSlot",
+	"NeckSlot",
+	"ShoulderSlot",
+	"!Skip!", -- Shirt
+	"ChestSlot",
+	"WaistSlot",
+	"LegsSlot",
+	"FeetSlot",
+	"WristSlot",
+	"HandsSlot",
+	"Finger0Slot",
+	"Finger1Slot",
+	"Trinket0Slot",
+	"Trinket1Slot",
+	"BackSlot",
+	"MainHandSlot",
+	"SecondaryHandSlot"
+}
 
 local f = CreateFrame("Frame", nil, PaperDollFrame)
 f:RegisterEvent("PLAYER_ENTERING_WORLD")
 
+-- Tooltip and scanning by Phanx @ http://www.wowinterface.com/forums/showthread.php?p=271406
+local S_ITEM_LEVEL = "^" .. gsub(ITEM_LEVEL, "%%d", "(%%d+)")
+
+local scantip = CreateFrame("GameTooltip", "BenikUIiLvlScanningTooltip", nil, "GameTooltipTemplate")
+scantip:SetOwner(UIParent, "ANCHOR_NONE")
+
+local function getItemLevel(slotId)
+	local hasItem = scantip:SetInventoryItem("player", slotId)
+	local realItemLevel
+	if not hasItem then return nil end
+
+	for i = 2, scantip:NumLines() do
+		local text = _G["BenikUIiLvlScanningTooltipTextLeft"..i]:GetText()
+		if text and text ~= "" then
+			realItemLevel = realItemLevel or match(text, S_ITEM_LEVEL)
+			if realItemLevel then
+				return realItemLevel
+			end
+		end
+	end
+end
+
 function BUI:update_iLevelItems()
 	local db = E.db.benikui.misc.ilevel
 	for i = 1, 17 do
-		if i ~= 4 then
-			local itemLink = GetInventoryItemLink("player", i)	
+		local itemLink = GetInventoryItemLink("player", i)
+		local iLvl = getItemLevel(i)
+		if i ~= 4 and (equipped[i] ~= itemLink or f[i]:GetText() ~= nil) then
+			equipped[i] = itemLink
 			if (itemLink ~= nil) then
-				local iLvl = GetDetailedItemLevelInfo(itemLink)
 				f[i]:SetText(iLvl)
 				local _, _, ItemRarity = GetItemInfo(itemLink)
 				if ItemRarity and db.colorStyle == 'RARITY' then
@@ -36,42 +80,44 @@ function BUI:update_iLevelItems()
 					f[i]:SetTextColor(BUI:unpackColor(db.color))
 				end
 			else
-				f[i]:SetText('')
+				f[i]:SetText("")
 			end
 			f[i]:FontTemplate(LSM:Fetch('font', db.font), db.fontsize, db.fontflags)
 		end
 	end
 end
 
-local function createString(parent, myPoint, parentPoint, x, y)
-	local s = f:CreateFontString(nil, "OVERLAY")
-	s:FontTemplate()
-	s:SetPoint(myPoint, parent, parentPoint, x or 0, y or 0)
-	return s
+local function returnPoints(number)
+	if number <= 5 or number == 15 or number == 9 then -- Left side
+		return "BOTTOMLEFT", "BOTTOMLEFT", 0, 1
+	elseif number <= 14 then -- Right side
+		return "BOTTOMRIGHT", "BOTTOMRIGHT", 2, 1
+	else -- Weapon slots
+		return "BOTTOM", "BOTTOM", 2, 1
+	end
 end
 
-local function applyStrings()
+local function anchorString()
+	for i = 1, 17 do
+		if i ~= 4 then
+			local parent = _G["Character"..slotTable[i]]
+			local myPoint, parentPoint, x, y = returnPoints(i)
+			f[i]:ClearAllPoints()
+			f[i]:Point(myPoint, parent, parentPoint, x or 0, y or 0)
+		end
+	end
+end
+
+local function createString()
+	for i = 1, 17 do
+		if i ~= 4 then
+			f[i] = f:CreateFontString(nil, "OVERLAY")
+			f[i]:FontTemplate()
+		end
+	end
+
+	anchorString(f)
 	f:SetFrameLevel(CharacterHeadSlot:GetFrameLevel())
-
-	f[1] = createString(CharacterHeadSlot, "BOTTOMLEFT", "BOTTOMLEFT", xo, yo)
-	f[2] = createString(CharacterNeckSlot, "BOTTOMLEFT", "BOTTOMLEFT", xo, yo)
-	f[3] = createString(CharacterShoulderSlot, "BOTTOMLEFT", "BOTTOMLEFT", xo, yo)
-	f[15] = createString(CharacterBackSlot, "BOTTOMLEFT", "BOTTOMLEFT", xo, yo)
-	f[5] = createString(CharacterChestSlot, "BOTTOMLEFT", "BOTTOMLEFT", xo, yo)
-	f[9] = createString(CharacterWristSlot, "BOTTOMLEFT", "BOTTOMLEFT", xo, yo)
-
-	f[10] = createString(CharacterHandsSlot, "BOTTOMRIGHT", "BOTTOMRIGHT", 3, yo)
-	f[6] = createString(CharacterWaistSlot, "BOTTOMRIGHT", "BOTTOMRIGHT", 3, yo)
-	f[7] = createString(CharacterLegsSlot, "BOTTOMRIGHT", "BOTTOMRIGHT", 3, yo)
-	f[8] = createString(CharacterFeetSlot, "BOTTOMRIGHT", "BOTTOMRIGHT", 3, yo)
-	f[11] = createString(CharacterFinger0Slot, "BOTTOMRIGHT", "BOTTOMRIGHT", 3, yo)
-	f[12] = createString(CharacterFinger1Slot, "BOTTOMRIGHT", "BOTTOMRIGHT", 3, yo)
-	f[13] = createString(CharacterTrinket0Slot, "BOTTOMRIGHT", "BOTTOMRIGHT", 3, yo)
-	f[14] = createString(CharacterTrinket1Slot, "BOTTOMRIGHT", "BOTTOMRIGHT", 3, yo)
-
-	f[16] = createString(CharacterMainHandSlot, "BOTTOMLEFT", "BOTTOMLEFT", 0, yo)
-	f[17] = createString(CharacterSecondaryHandSlot, "BOTTOMRIGHT", "BOTTOMRIGHT", 3, yo)
-
 	f:Hide()
 end
 
@@ -80,7 +126,7 @@ local function OnEvent(self, event)
 	if event == "PLAYER_ENTERING_WORLD" then
 		self:UnregisterEvent(event)
 
-		applyStrings()
+		createString()
 
 		PaperDollFrame:HookScript("OnShow", function(self)
 			f:RegisterEvent("PLAYER_EQUIPMENT_CHANGED")
