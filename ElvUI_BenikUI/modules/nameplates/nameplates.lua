c34b676fb572315fc34f64f29b46f9676510639d
Benik <benik@otenet.gr>
2018-04-07 17:21:27 +0300

    Added nameplate shadows

---
+++ ElvUI_BenikUI/modules/nameplates/nameplates.lua
@@ -0,0 +1,30 @@
+local E, L, V, P, G = unpack(ElvUI);
+local NP = E:GetModule('NamePlates')
+local mod = E:NewModule('BuiNameplates', 'AceHook-3.0');
+
+function mod:NameplateShadows(frame, useMainFrame)
+	local parent = frame
+
+	if(parent:GetObjectType() == "Texture") then
+		parent = frame:GetParent()
+	end
+
+	if useMainFrame then
+		parent:CreateSoftShadow()
+		return
+	end
+
+	parent.backdrop:CreateSoftShadow()
+end
+
+
+function mod:Initialize()
+	if E.db.benikui.general.shadows ~= true then return end
+	hooksecurefunc(NP, 'StyleFrame', mod.NameplateShadows)
+end
+
+local function InitializeCallback()
+	mod:Initialize()
+end
+
+E:RegisterModule(mod:GetName(), InitializeCallback)
