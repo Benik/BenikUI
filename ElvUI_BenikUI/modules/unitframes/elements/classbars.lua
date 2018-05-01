a9bc78e788b1790fe9bd29082ad24bc17ed4bd8e
Benik <benik@otenet.gr>
2018-05-01 12:17:16 +0300

    Added a check to prevent errors

--- ElvUI_BenikUI/modules/unitframes/elements/classbars.lua
+++ ElvUI_BenikUI/modules/unitframes/elements/classbars.lua
@@ -14,22 +14,25 @@ function UFB:Configure_ClassBar(frame)
 		bars.backdrop:CreateSoftShadow()
 	end
 
-	frame.shadow:ClearAllPoints()
-	if frame.USE_MINI_CLASSBAR and not frame.CLASSBAR_DETACHED then
-		frame.shadow:Point('TOPLEFT', frame.Health.backdrop, 'TOPLEFT', -2, 2)
-		frame.shadow:Point('BOTTOMRIGHT', frame, 'BOTTOMRIGHT', 2, -2)
-		bars.backdrop.shadow:Show()
-	elseif not frame.CLASSBAR_DETACHED then
-		frame.shadow:SetOutside(frame, 2, 2)
-		bars.backdrop.shadow:Hide()
-	else
-		frame.shadow:SetOutside(frame, 2, 2)
-		bars.backdrop.shadow:Show()
+	if frame.shadow then
+		frame.shadow:ClearAllPoints()
+		if frame.USE_MINI_CLASSBAR and not frame.CLASSBAR_DETACHED then
+			frame.shadow:Point('TOPLEFT', frame.Health.backdrop, 'TOPLEFT', -2, 2)
+			frame.shadow:Point('BOTTOMRIGHT', frame, 'BOTTOMRIGHT', 2, -2)
+			bars.backdrop.shadow:Show()
+		elseif not frame.CLASSBAR_DETACHED then
+			frame.shadow:SetOutside(frame, 2, 2)
+			bars.backdrop.shadow:Hide()
+		else
+			frame.shadow:SetOutside(frame, 2, 2)
+			bars.backdrop.shadow:Show()
+		end
 	end
 
 	if (frame.ClassBar == 'ClassIcons' or frame.ClassBar == 'Runes') then
-		local maxClassBarButtons = max(UF.classMaxResourceBar[E.myclass] or 0, MAX_COMBO_POINTS)
-		for i = 1, maxClassBarButtons do
+		local maxBars = max(UF['classMaxResourceBar'][E.myclass] or 0, MAX_COMBO_POINTS)
+		for i = 1, maxBars do
+			print('BEEP')
 			if not bars[i].backdrop.shadow then
 				bars[i].backdrop:CreateSoftShadow()
 			end
@@ -37,3 +40,4 @@ function UFB:Configure_ClassBar(frame)
 	end
 end
 hooksecurefunc(UF, "Configure_ClassBar", UFB.Configure_ClassBar)
+
