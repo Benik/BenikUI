a3a1049c8dc7bb99252335ef387dae9971d82b62
Benik <benik@otenet.gr>
2018-07-18 15:05:38 +0300

    Fixed SplashScreen daily check

--- ElvUI_BenikUI/core/splashScreen.lua
+++ ElvUI_BenikUI/core/splashScreen.lua
@@ -66,10 +66,12 @@ function BUI:SplashScreen()
 	CreateSplashScreen()
 
 	local db = E.private.benikui.session
-	local _, _, day = C_Calendar_GetDate()
-	if day == db.day then return end
+	local date = C_Calendar_GetDate()
+	local presentWeekday = date.weekday;
+	print(presentWeekday, db.day)
+	if presentWeekday == db.day then return end
 
 	-- Show Splash Screen only if the install is completed
 	if E.db.benikui.installed == true then C_TimerAfter(6, ShowSplashScreen) end
-	db.day = day
+	db.day = presentWeekday
 end
