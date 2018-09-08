fe06c4f4bc509dca3d110ea9b78c2b90b0393c40
Benik <benik@otenet.gr>
2018-09-08 17:22:51 +0300

    BigWigs profile creation is really fecked up from their side. There will be a BenikUI profile only for this case. Cannot be overitten by the Install or the options. Any further changes must be done manually from BigWigs Profile options

--- ElvUI_BenikUI/core/addonProfiles/BigWigs.lua
+++ ElvUI_BenikUI/core/addonProfiles/BigWigs.lua
@@ -3,6 +3,8 @@ local BUI = E:GetModule('BenikUI');
 
 function BUI:LoadBigWigsProfile()
 	local font, fontsize
+	local key = BUI.Title
+
 	if E.private.benikui.expressway == true then
 		font = "Expressway"
 		fontsize = 11
@@ -13,12 +15,12 @@ function BUI:LoadBigWigsProfile()
 
 	if BigWigs3DB['profiles'] == nil then BigWigs3DB['profiles'] = {} end
 
-	if BigWigs3DB['profiles'][BUI.AddonProfileKey] == nil then
+	if BigWigs3DB['profiles'][key] == nil then
 		BigWigs3DB = {
 			["namespaces"] = {
 				["BigWigs_Plugins_Alt Power"] = {
 					["profiles"] = {
-						[BUI.AddonProfileKey] = {
+						[key] = {
 							["fontName"] = font,
 							["fontOutline"] = "",
 							["fontsize"] = fontsize,
@@ -27,7 +29,7 @@ function BUI:LoadBigWigsProfile()
 				},
 				["BigWigs_Plugins_Bars"] = {
 					["profiles"] = {
-						[BUI.AddonProfileKey] = {
+						[key] = {
 							["fontName"] = font,
 							["BigWigsAnchor_width"] = 199.999908447266,
 							["texture"] = "BuiFlat",
@@ -37,14 +39,14 @@ function BUI:LoadBigWigsProfile()
 				},
 				["BigWigs_Plugins_Super Emphasize"] = {
 					["profiles"] = {
-						[BUI.AddonProfileKey] = {
+						[key] = {
 							["fontName"] = font,
 						},
 					},
 				},
 				["BigWigs_Plugins_Messages"] = {
 					["profiles"] = {
-						[BUI.AddonProfileKey] = {
+						[key] = {
 							["fontSize"] = 20,
 							["fontName"] = font,
 						},
@@ -52,7 +54,7 @@ function BUI:LoadBigWigsProfile()
 				},
 				["BigWigs_Plugins_Proximity"] = {
 					["profiles"] = {
-						[BUI.AddonProfileKey] = {
+						[key] = {
 							["fontSize"] = 20,
 							["fontName"] = font,
 							["width"] = 139.999984741211,
@@ -64,13 +66,10 @@ function BUI:LoadBigWigsProfile()
 				},
 			},
 			["profiles"] = {
-				[BUI.AddonProfileKey] = {
+				[key] = {
 				},
 			},
 		}
-
-		local db = LibStub("AceDB-3.0"):New(BigWigs3DB, nil, true)
-		db:SetProfile(BUI.AddonProfileKey)
 
 		if BUI.isInstallerRunning == false then -- don't print during Install, when applying profile that doesn't exist
 			print(BUI.profileStrings[1]..L['BigWigs'])
@@ -78,4 +77,9 @@ function BUI:LoadBigWigsProfile()
 	else
 		print(BUI.Title.."- "..L['BigWigs']..BUI.profileStrings[2])
 	end
+
+	LoadAddOn("BigWigs_Options")
+	LoadAddOn("BigWigs")
+
+	BigWigs.db:SetProfile(key)
 end
