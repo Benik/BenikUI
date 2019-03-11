b592fbe39a795e759bc29366aa959093a942acb3
Benik <benik@otenet.gr>
2019-03-11 22:04:56 +0200

    Nameplate Shadows

--- ElvUI_BenikUI/modules/nameplates/nameplates.lua
+++ ElvUI_BenikUI/modules/nameplates/nameplates.lua
@@ -3,16 +3,28 @@ local BUI = E:GetModule('BenikUI');
 local NP = E:GetModule('NamePlates')
 local mod = E:NewModule('BuiNameplates', 'AceHook-3.0');
 
-function mod:NameplateShadows(frame)
-	if not frame.Health.backdrop.shadow then
-		frame.Health.backdrop:CreateSoftShadow()
+function mod:NameplateShadows(nameplate)
+	if not nameplate.Health.backdrop.shadow then
+		nameplate.Health.backdrop:CreateSoftShadow()
+	end
+	if not nameplate.Power.backdrop.shadow then
+		nameplate.Power.backdrop:CreateSoftShadow()
+	end
+	if not nameplate.Castbar.backdrop.shadow then
+		nameplate.Castbar.backdrop:CreateSoftShadow()
+	end
+	if not nameplate.Castbar.Button.shadow then
+		nameplate.Castbar.Button:CreateSoftShadow()
+	end
+	if not nameplate.Portrait.backdrop.shadow then
+		nameplate.Portrait.backdrop:CreateSoftShadow()
 	end
 end
 
 
 function mod:Initialize()
 	if not BUI.ShadowMode then return end
-	--hooksecurefunc(NP, 'UpdatePlate', mod.NameplateShadows)
+	hooksecurefunc(NP, 'StylePlate', mod.NameplateShadows)
 end
 
 local function InitializeCallback()
