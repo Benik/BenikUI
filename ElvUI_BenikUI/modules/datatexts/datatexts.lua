753b15b6eb0b65b1b4549e2dc18d48b302bbd62a
Simpy <kodi03466@hotmail.com>
2020-08-26 03:54:18 -0400

    panelName wont exist here, blacklist was always nil

--- ElvUI_BenikUI/modules/datatexts/datatexts.lua
+++ ElvUI_BenikUI/modules/datatexts/datatexts.lua
@@ -2,21 +2,17 @@ local BUI, E, L, V, P, G = unpack(select(2, ...))
 local DT = E:GetModule('DataTexts')
 local mod = BUI:NewModule('DataTexts', 'AceEvent-3.0');
 
-local blacklist = panelName == _G.LocPlusLeftDT or panelName == _G.LocPlusRightDT or panelName == _G.MinimapPanel or panelName == _G.LeftChatDataPanel or panelName == _G.RightChatDataPanel
-
-function mod:BuildPanelFrame(name, db)
-	db = db or E.global.datatexts.customPanels[name] or DT:Panel_DefaultGlobalSettings(name)
-
+function mod:BuildPanelFrame(name)
 	local Panel = DT:FetchFrame(name)
 	Panel:Style('Outside')
 end
 
-function mod:UpdatePanelInfo(panelName, panel, ...)
+function mod:UpdatePanelInfo(panelName, panel)
 	if not panel then panel = DT.RegisteredPanels[panelName] end
 	local db = panel.db or P.datatexts.panels[panelName] and DT.db.panels[panelName]
 	if not db then return end
 
-	if not blacklist then
+	if not (panelName == _G.LocPlusLeftDT or panelName == _G.LocPlusRightDT or panelName == _G.MinimapPanel or panelName == _G.LeftChatDataPanel or panelName == _G.RightChatDataPanel) then
 		panel:Style('Outside')
 		if db.benikuiStyle then
 			if panel.style then
@@ -25,7 +21,7 @@ function mod:UpdatePanelInfo(panelName, panel, ...)
 				panel.style:Hide()
 			end
 		end
-		
+
 		if BUI.ShadowMode then
 			if not (db.border and db.backdrop or db.backdrop) then
 				panel.shadow:Hide()
@@ -36,7 +32,7 @@ function mod:UpdatePanelInfo(panelName, panel, ...)
 	end
 end
 
-function mod:SetupTooltip(panel)
+function mod:SetupTooltip()
 	DT.tooltip:Style('Outside')
 end
 
