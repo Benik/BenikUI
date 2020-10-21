ea18cfa1d49e42250497c60bc46009f70862a247
Benik <benik@otenet.gr>
2020-10-21 17:55:08 +0300

    Fixed(?) locales not showing during install

--- ElvUI_BenikUI/core/install.lua
+++ ElvUI_BenikUI/core/install.lua
@@ -1,4 +1,5 @@
-local BUI, E, L, V, P, G = unpack(select(2, ...))
+local BUI, E, _, V, P, G = unpack(select(2, ...))
+local L = E.Libs.ACL:GetLocale('ElvUI', E.global.general.locale or 'enUS');
 local format, checkTable = format, next
 local tinsert, twipe, tsort, tconcat = table.insert, table.wipe, table.sort, table.concat
 local _G = _G
