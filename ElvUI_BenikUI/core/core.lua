b0f4f9c2018662586ba83ccb16dc490432e4d25d
Benik <benik@otenet.gr>
2021-02-02 23:17:36 +0200

    Revert "Mera's top/bottom panels don't need style. They are pretty anyway :P"
    This reverts commit a6baea47a8ad5ffdb6ff0c64bcd6def364387eee.

--- ElvUI_BenikUI/core/core.lua
+++ ElvUI_BenikUI/core/core.lua
@@ -31,6 +31,7 @@ end
 
 -- Check other addons
 BUI.SLE = BUI:IsAddOnEnabled('ElvUI_SLE')
+BUI.MER = BUI:IsAddOnEnabled('ElvUI_MerathilisUI')
 BUI.PA = BUI:IsAddOnEnabled('ProjectAzilroka')
 BUI.LP = BUI:IsAddOnEnabled('ElvUI_LocPlus')
 BUI.NB = BUI:IsAddOnEnabled('ElvUI_NutsAndBolts')
