a4fa00848d31e77768dd976402de6dd9e6a32a11
Benik <benik@otenet.gr>
2021-11-02 21:27:35 +0200

    Remove an outdated event

--- ElvUI_BenikUI/modules/misc/ilevel.lua
+++ ElvUI_BenikUI/modules/misc/ilevel.lua
@@ -151,7 +151,6 @@ function mod:Initialize()
 	end)
 
 	mod:RegisterEvent("PLAYER_EQUIPMENT_CHANGED", mod.UpdateItemLevel)
-	mod:RegisterEvent("ITEM_UPGRADE_MASTER_UPDATE", mod.UpdateItemLevel)
 	mod:RegisterEvent("PLAYER_AVG_ITEM_LEVEL_UPDATE", mod.UpdateItemLevel)
 	mod:RegisterEvent("PLAYER_ENTERING_WORLD")
 end
