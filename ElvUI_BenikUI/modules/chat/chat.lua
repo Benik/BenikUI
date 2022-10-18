46e5e53ba2e141a29db07cc6c9b0d72c933e0ff6
Benik <benik@otenet.gr>
2022-10-18 22:18:20 +0300

    Chat DTs to have a look at

--- ElvUI_BenikUI/modules/chat/chat.lua
+++ ElvUI_BenikUI/modules/chat/chat.lua
@@ -17,6 +17,7 @@ local IsMouseButtonDown = IsMouseButtonDown
 
 -- Place the new chat frame
 function mod:UpdateEditboxAnchors()
+	E.db.benikui.datatexts = E.db.benikui.datatexts or {}
 	if E.db.benikui.datatexts.chat.enable ~= true then return end
 	for _, frameName in pairs(CHAT_FRAMES) do
 		local frame = _G[frameName..'EditBox']
