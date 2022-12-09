b9387527f238e3dd884bea07c6847cc87a6c28c8
roxanne <87175044+roxannefromhell@users.noreply.github.com>
2022-12-09 21:02:59 +0100

    Update fps.lua
    with cap at 60 fps and vsync , the max fps stays at 59 fps  .... drive me nuts to have a orange color for it XD

--- ElvUI_BenikUI/modules/dashboard/panels/fps.lua
+++ ElvUI_BenikUI/modules/dashboard/panels/fps.lua
@@ -138,9 +138,9 @@ local function OnUpdate(self, elapsed)
 		local fpscolor
 		self.Status:SetValue(value)
 
-		if(value * 100 / max >= 60) then
+		if(value * 100 / max >= 59) then
 			fpscolor = 1
-		elseif value * 100 / max < 60 and value * 100 / max > 30 then
+		elseif value * 100 / max < 59 and value * 100 / max > 30 then
 			fpscolor = 2
 		else
 			fpscolor = 3
