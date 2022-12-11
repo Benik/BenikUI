848807d72c7d977b92049a3f2788438839404adc
Benik <benik@otenet.gr>
2022-12-11 13:35:31 +0200

    Added Lucky and Dlarge in the Coders list

--- ElvUI_BenikUI/core/optionsInfo.lua
+++ ElvUI_BenikUI/core/optionsInfo.lua
@@ -82,11 +82,13 @@ local CODING = {
 	'Hydra',
 	'Merathilis',
 	'Repooc',
+	'Luckyone',
 	'Pristie (frFR)',
 	'|cffff005athurin|r',
 	'|TInterface/AddOns/ElvUI/Core/Media/ChatLogos/Hibiscus:15:15:0:0:64:64:5:59:5:59|t Simpy',
 	'Cara (ptBR)',
 	'nadugi (koKR)',
+	'Dlarge (deDE)',
 }
 tsort(CODING, function(a, b) return E:StripString(a) < E:StripString(b) end)
 local CODING_STRING = tconcat(CODING, ", ")
