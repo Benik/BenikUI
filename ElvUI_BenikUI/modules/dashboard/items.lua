1c604ab4fa0a5cc878e4965d38036406964f57f7
Benik <benik@otenet.gr>
2023-01-27 15:56:04 +0200

    Items Dashboard: Changed the events needed

--- ElvUI_BenikUI/modules/dashboard/items.lua
+++ ElvUI_BenikUI/modules/dashboard/items.lua
@@ -212,7 +212,8 @@ function mod:ToggleItems()
 
 	if db.items.enable then
 		E:EnableMover(holder.mover.name)
-		mod:RegisterEvent('BAG_UPDATE_DELAYED', mod.GetUserItems)
+		mod:RegisterEvent('BAG_UPDATE', mod.GetUserItems)
+		mod:RegisterEvent('CHAT_MSG_LOOT', mod.UpdateItems)
 
 		mod:ToggleStyle(holder, 'items')
 		mod:ToggleTransparency(holder, 'items')
@@ -221,7 +222,8 @@ function mod:ToggleItems()
 		holder:SetScript('OnLeave', holderOnLeave)
 	else
 		E:DisableMover(holder.mover.name)
-		mod:UnregisterEvent('BAG_UPDATE_DELAYED')
+		mod:UnregisterEvent('BAG_UPDATE')
+		mod:UnregisterEvent('CHAT_MSG_LOOT')
 
 		holder:SetScript('OnEnter', nil)
 		holder:SetScript('OnLeave', nil)
