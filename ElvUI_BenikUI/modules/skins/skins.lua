87651e866f620a75f33d2b41ad9fd455812ddcf6
Benik <benik@otenet.gr>
2023-05-09 15:27:54 +0300

    Prevent a Priest error. Credit: cxmplex

--- ElvUI_BenikUI/modules/skins/skins.lua
+++ ElvUI_BenikUI/modules/skins/skins.lua
@@ -12,15 +12,19 @@ function mod:HandleCloseButton(f)
 	end
 
 	f:HookScript('OnEnter', function(self)
-		if E.myclass == 'PRIEST' then
-			self.Texture:SetVertexColor(unpack(E["media"].rgbvaluecolor))
-		else
-			self.Texture:SetVertexColor(classColor.r, classColor.g, classColor.b)
+		if self.Texture then
+			if E.myclass == 'PRIEST' then
+				self.Texture:SetVertexColor(unpack(E["media"].rgbvaluecolor))
+			else
+				self.Texture:SetVertexColor(classColor.r, classColor.g, classColor.b)
+			end
 		end
 	end)
 
 	f:HookScript('OnLeave', function(self)
-		self.Texture:SetVertexColor(1, 1, 1)
+		if self.Texture then
+			self.Texture:SetVertexColor(1, 1, 1)
+		end
 	end)
 end
 hooksecurefunc(S, "HandleCloseButton", mod.HandleCloseButton)
