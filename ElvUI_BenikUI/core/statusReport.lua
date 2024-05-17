5f2be5113fafed9c42b249b3c1dd84219166bac0
thurin <thurin@gmail.com>
2024-05-16 21:27:59 -0600

    actually no reason to override the text if just using default

--- ElvUI_BenikUI/core/statusReport.lua
+++ ElvUI_BenikUI/core/statusReport.lua
@@ -1,19 +1,5 @@
 local BUI, E, L, V, P, G = unpack((select(2, ...)))
 
-local GetAddOnInfo = GetAddOnInfo
-local GetNumAddOns = GetNumAddOns
-
-local function AreOtherAddOnsEnabled()
-	local name
-	for i = 1, GetNumAddOns() do
-		name = GetAddOnInfo(i)
-		if ((name ~= "ElvUI" and name ~= "ElvUI_Options" and name ~= 'ElvUI_Libraries' and name ~= "ElvUI_BenikUI") and E:IsAddOnEnabled(name)) then --Loaded or load on demand
-			return "Yes"
-		end
-	end
-	return "No"
-end
-
 local function CreateStatusFrame()
 	local StatusFrame = ElvUIStatusReport
 
@@ -31,10 +17,6 @@ local function CreateStatusFrame()
 	StatusFrame.TitleLogoFrame.Title:SetJustifyH("CENTER")
 	StatusFrame.TitleLogoFrame.Title:SetJustifyV("MIDDLE")
 	StatusFrame.TitleLogoFrame.Title:SetFormattedText("|cfffe7b2c- ElvUI Status Report -|r")
-
-	-- Content lines
-	local addons, plugins = E:AreOtherAddOnsEnabled()
-	StatusFrame.Section1.Content.Line2.Text:SetFormattedText('Other AddOns Enabled: |cff%s|r', (not addons and plugins and 'ff9933Plugins') or (addons and 'ff3333Yes') or '33ff33No')
 end
 
 hooksecurefunc(E, "UpdateStatusFrame", CreateStatusFrame)
