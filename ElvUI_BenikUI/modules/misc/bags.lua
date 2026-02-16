6e1358f16d7d554fb066d3f042b916b131994e60
Benik <benik.mobile@gmail.com>
2026-02-16 23:58:27 +0200

    Updated the bags file

--- ElvUI_BenikUI/modules/misc/bags.lua
+++ ElvUI_BenikUI/modules/misc/bags.lua
@@ -3,49 +3,60 @@ local mod = BUI:GetModule('Bags')
 local B = E:GetModule('Bags')
 
 local _G = _G
+local hooksecurefunc = hooksecurefunc
+local next = next
 local NUM_CONTAINER_FRAMES = NUM_CONTAINER_FRAMES
 
 -- GLOBALS: hooksecurefunc
 
 local SPACING = E.Spacing
-local BORDER = E.Border;
+local BORDER = E.Border
 
 local function SellFrame()
 	if E.db.benikui.general.benikuiStyle ~= true then return end
-	if _G.ElvUIVendorGraysFrame then
-		_G.ElvUIVendorGraysFrame:BuiStyle('Outside')
+	local elvuiVendorGraysFrame = _G.ElvUIVendorGraysFrame
+
+	if elvuiVendorGraysFrame then
+		elvuiVendorGraysFrame:BuiStyle('Outside')
 	end
 end
 hooksecurefunc(B, "CreateSellFrame", SellFrame)
 
 local function StyleBags()
-	if _G.ElvUI_ContainerFrame then
-		_G.ElvUI_ContainerFrame:BuiStyle('Outside')
-		_G.ElvUI_ContainerFrameContainerHolder:BuiStyle('Outside')
+	local elvuiContainerFrame = _G.ElvUI_ContainerFrame
+	local elvuiContainerFrameContainerHolder = _G.ElvUI_ContainerFrameContainerHolder
+	local elvuiBags = _G.ElvUIBags
+
+	if elvuiContainerFrame then
+		elvuiContainerFrame:BuiStyle('Outside')
 	end
 
-	if _G.ElvUI_ContainerFrameContainerHolder then
-		_G.ElvUI_ContainerFrameContainerHolder:Point('BOTTOMLEFT', _G.ElvUI_ContainerFrame.style, 'TOPLEFT', 0, SPACING + BORDER)
+	if elvuiContainerFrameContainerHolder then
+		elvuiContainerFrameContainerHolder:BuiStyle('Outside')
+		elvuiContainerFrameContainerHolder:Point('BOTTOMLEFT', elvuiContainerFrame.style, 'TOPLEFT', 0, SPACING + BORDER)
 	end
 
-	if _G.ElvUIBags then
-		_G.ElvUIBags.backdrop:BuiStyle('Outside')
-		_G.ElvUIBags.backdrop:SetTemplate('Transparent')
+	if elvuiBags then
+		elvuiBags.backdrop:BuiStyle('Outside')
+		elvuiBags.backdrop:SetTemplate('Transparent')
 	end
 end
 
 local function StyleBankBags()
-	if _G.ElvUI_BankContainerFrame then
-		_G.ElvUI_BankContainerFrame:BuiStyle('Outside')
-		_G.ElvUI_BankContainerFrameBankTabs:BuiStyle('Outside')
+	local elvuiBankContainerFrame = _G.ElvUI_BankContainerFrame
+	if elvuiBankContainerFrame then
+		elvuiBankContainerFrame:BuiStyle('Outside')
 	end
 
-	if _G.ElvUI_BankContainerFrameBankTabs then
-		_G.ElvUI_BankContainerFrameBankTabs:Point('BOTTOMLEFT', _G.ElvUI_BankContainerFrame.style, 'TOPLEFT', 0, SPACING + BORDER)
+	local elvuiBankContainerFrameBankTabs = _G.ElvUI_BankContainerFrameBankTabs
+	if elvuiBankContainerFrameBankTabs then
+		elvuiBankContainerFrameBankTabs:BuiStyle('Outside')
+		elvuiBankContainerFrameBankTabs:Point('BOTTOMLEFT', elvuiBankContainerFrame.style, 'TOPLEFT', 0, SPACING + BORDER)
 	end
 
-	if _G.ElvUI_BankContainerFrameWarbandTabs then
-		_G.ElvUI_BankContainerFrameWarbandTabs:BuiStyle('Outside')
+	local elvuiBankContainerFrameWarbandTabs = _G.ElvUI_BankContainerFrameWarbandTabs
+	if elvuiBankContainerFrameWarbandTabs then
+		elvuiBankContainerFrameWarbandTabs:BuiStyle('Outside')
 	end
 end
 
@@ -65,21 +76,27 @@ local function StyleAllBags()
 			container:BuiStyle('Inside')
 		end
 	end
-	if _G.BankFrame then
-		_G.BankFrame:BuiStyle('Outside')
+
+	local bankFrame = _G.BankFrame
+	if bankFrame then
+		bankFrame:BuiStyle('Outside')
 	end
 end
 
 local function StyleBagBar()
 	if not E.private.bags.bagBar then return end
 
-	_G.ElvUIBagBar.backdrop:BuiStyle('Outside')
+	local elvuiBagBar = _G.ElvUIBagBar
+	elvuiBagBar.backdrop:BuiStyle('Outside')
 
-	if BUI.ShadowMode then
-		_G.MainMenuBarBackpackButton:CreateSoftShadow()
-		for i, button in ipairs(B.BagBar.buttons) do
-			button:CreateSoftShadow()
-		end
+	if not BUI.ShadowMode then return end
+
+	local mainMenuBarBackpackButton = _G.MainMenuBarBackpackButton
+	mainMenuBarBackpackButton:CreateSoftShadow()
+
+	local buttons = B.BagBar.buttons
+	for _, button in next, buttons do
+		button:CreateSoftShadow()
 	end
 end
 
@@ -91,6 +108,7 @@ end
 
 function mod:Initialize()
 	if E.db.benikui.general.benikuiStyle ~= true then return end
+
 	AllInOneBags()
 	StyleAllBags()
 	StyleBankBags()
