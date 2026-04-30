d5d7afcebd814b02aec07d0ea70412aedc34bcd2
Benik <benik.mobile@gmail.com>
2026-04-30 17:30:38 +0300

    Added BigWigs bars and updated the profile

--- ElvUI_BenikUI/modules/skins/BigWigs.lua
+++ ElvUI_BenikUI/modules/skins/BigWigs.lua
@@ -1,160 +1,112 @@
 local BUI, E, L, V, P, G = unpack((select(2, ...)))
+local S = E:GetModule('Skins')
+local mod = BUI:GetModule('Skins')
 
---[[if not BUI.AS then return end
-local AS = unpack(AddOnSkins)
+function mod:BigWigs()
+	local shadowsEnabled = (E.db.benikui.general.benikuiStyle and E.db.benikui.general.shadows)
 
-if not AS:CheckAddOn('BigWigs') then return end
-
-function AS:BigWigs(event, addon)
-	if event == 'PLAYER_ENTERING_WORLD' then
-		if BigWigsLoader then
-			BigWigsLoader.RegisterMessage('AddOnSkins', "BigWigs_FrameCreated", function(event, frame, name)
-				if name == "QueueTimer" then
-					AS:SkinStatusBar(frame, {1, 0, 0})
-					frame:ClearAllPoints()
-					frame:SetPoint('TOP', '$parent', 'BOTTOM', 0, AS:AdjustForTheme(-2))
-					frame:SetHeight(16)
-				end
-			end)
-		end
-		return
-	end
-
-	if event == 'ADDON_LOADED' and addon == 'BigWigs_Plugins' then
-		if E.db.benikui.general.shadows and E.db.benikui.skins.addonSkins.bigwigs then
-			BigWigsInfoBox:CreateSoftShadow()
-			--BigWigsAltPower:CreateSoftShadow()
+	local function removeStyle(bar)
+		local bd = bar.candyBarBackdrop
+		bd:Hide()
+		if bd.iborder then
+			bd.iborder:Hide()
+			bd.oborder:Hide()
 		end
 
-		local function FreeStyle(bar)
-			local bd = bar.candyBarBackdrop
+		local restore = bar:Get("bigwigs:restoreicon")
+		if restore then
 			local iconBd = bar.candyBarIconFrameBackdrop
-			bd:CreateSoftShadow()
-			iconBd:CreateSoftShadow()
-
-			local height = bar:Get("bigwigs:restoreheight")
-			if height then
-				bar:SetHeight(height)
-			end
-
-			local tex = bar:Get("bigwigs:restoreicon")
-			if tex then
-				bar:SetIcon(tex)
-				bar:Set("bigwigs:restoreicon", nil)
-			end
-
-			bd:Hide()
 			iconBd:Hide()
-			if AS:CheckOption('Theme') == 'ThickBorder' or AS:CheckOption('Theme') == 'TwoPixel' then
-				bd.InsideBorder:Hide()
-				bd.OutsideBorder:Hide()
-				iconBd.InsideBorder:Hide()
-				iconBd.OutsideBorder:Hide()
+			if iconBd.iborder then
+				iconBd.iborder:Hide()
+				iconBd.oborder:Hide()
 			end
 		end
-
-		local function ApplyStyle(bar)
-			local bd = bar.candyBarBackdrop
-
-			AS:SetTemplate(bd)
-			bd:SetOutside(bar)
-
-			local tex = bar:GetIcon()
-			if tex then
-				local icon = bar.candyBarIconFrame
-				bar:SetIcon(nil)
-				icon:SetTexture(tex)
-				icon:Show()
-				icon:ClearAllPoints()
-				icon:SetPoint('BOTTOMRIGHT', bar, 'BOTTOMLEFT', -7, 0)
-				icon:SetSize(bar:GetHeight(), bar:GetHeight())
-				bar:Set("bigwigs:restoreicon", tex)
-				AS:SkinTexture(icon)
-
-				local iconBd = bar.candyBarIconFrameBackdrop
-				AS:SetTemplate(iconBd)
-				iconBd:SetOutside(icon)
-
-				iconBd:Show()
-				if AS:CheckOption('Theme') == 'ThickBorder' or AS:CheckOption('Theme') == 'TwoPixel' then
-					iconBd.InsideBorder:SetShown(AS:CheckOption('Theme') == 'ThickBorder' or AS:CheckOption('Theme') == 'TwoPixel')
-					iconBd.OutsideBorder:SetShown(AS:CheckOption('Theme') == 'ThickBorder')
-				end
-			end
-
-			bd:Show()
-			if AS:CheckOption('Theme') == 'ThickBorder' or AS:CheckOption('Theme') == 'TwoPixel' then
-				bd.InsideBorder:SetShown(AS:CheckOption('Theme') == 'ThickBorder' or AS:CheckOption('Theme') == 'TwoPixel')
-				bd.OutsideBorder:SetShown(AS:CheckOption('Theme') == 'ThickBorder')
-			end]
-		end
-
-		local function ApplyStyleHalfBar(bar)
-			local bd = bar.candyBarBackdrop
-
-			AS:SetTemplate(bd)
-			bd:SetOutside(bar)
-			bd:CreateSoftShadow()
-			
-			local tex = bar:GetIcon()
-			if tex then
-				local icon = bar.candyBarIconFrame
-				bar:SetIcon(nil)
-				icon:SetTexture(tex)
-				icon:Show()
-				icon:ClearAllPoints()
-				icon:SetPoint('BOTTOMRIGHT', bar, 'BOTTOMLEFT', -7, 0)
-				icon:SetSize(bar:GetHeight() * 2, bar:GetHeight() * 2)
-				bar:Set("bigwigs:restoreicon", tex)
-				AS:SkinTexture(icon)
-
-				local iconBd = bar.candyBarIconFrameBackdrop
-				AS:SetTemplate(iconBd)
-				iconBd:SetOutside(icon)
-				iconBd:CreateSoftShadow()
-
-				iconBd:Show()
-				if AS:CheckOption('Theme') == 'ThickBorder' or AS:CheckOption('Theme') == 'TwoPixel' then
-					iconBd.InsideBorder:SetShown(AS:CheckOption('Theme') == 'ThickBorder' or AS:CheckOption('Theme') == 'TwoPixel')
-					iconBd.OutsideBorder:SetShown(AS:CheckOption('Theme') == 'ThickBorder')
-				end
-			end
-
-			bar.candyBarLabel:ClearAllPoints()
-			bar.candyBarLabel:Point("LEFT", bar, "LEFT", 2, AS:AdjustForTheme(14))
-			bar.candyBarLabel:Point("RIGHT", bar, "RIGHT", -2, AS:AdjustForTheme(14))
-
-			bar.candyBarDuration:ClearAllPoints()
-			bar.candyBarDuration:Point("LEFT", bar, "LEFT", 2, AS:AdjustForTheme(14))
-			bar.candyBarDuration:Point("RIGHT", bar, "RIGHT", -2, AS:AdjustForTheme(14))
-
-			bd:Show()
-			if AS:CheckOption('Theme') == 'ThickBorder' or AS:CheckOption('Theme') == 'TwoPixel' then
-				bd.InsideBorder:SetShown(AS:CheckOption('Theme') == 'ThickBorder' or AS:CheckOption('Theme') == 'TwoPixel')
-				bd.OutsideBorder:SetShown(AS:CheckOption('Theme') == 'ThickBorder')
-			end
-		end
-
-		_G.BigWigsAPI:RegisterBarStyle('AddOnSkins', {
-			apiVersion = 1, version = 10,
-			barSpacing = 3,
-			barHeight = 20,
-			ApplyStyle = function(bar) ApplyStyle(bar) end,
-			BarStopped = function(bar) FreeStyle(bar) end,
-			GetStyleName = function() return 'AddOnSkins' end,
-		})
-		_G.BigWigsAPI:RegisterBarStyle('AddOnSkins Half-Bar', {
-			apiVersion = 1, version = 10,
-			barSpacing = 13,
-			barHeight = 10,
-			ApplyStyle = function(bar) ApplyStyleHalfBar(bar) end,
-			BarStopped = function(bar) FreeStyle(bar) end,
-			GetStyleName = function() return 'AddOnSkins Half-Bar' end,
-		})
-
-		AS:UnregisterSkinEvent('BigWigs', event)
 	end
-end
 
-AS:RegisterSkin('BigWigs', AS.BigWigs, 'ADDON_LOADED')
-AS:RegisterSkinForPreload('BigWigs_Plugins', AS.BigWigs, 'BigWigs')]]
+	local function styleBar(bar)
+		local bd = bar.candyBarBackdrop
+		bd:ClearAllPoints()
+
+		bd:SetTemplate("Transparent")
+		bd:SetOutside(bar)
+
+		if not E.PixelMode and bd.iborder then
+			bd.iborder:Show()
+			bd.oborder:Show()
+		end
+
+		if shadowsEnabled then
+			bd:CreateSoftShadow()
+		end
+
+		bd:Show()
+
+		local iconTexture = bar:GetIcon()
+		if iconTexture then
+			local reApplyIcon = false
+			local statusbar = bar.candyBarBar
+			local iconFrame = bar.candyBarIconFrame
+			local iconBd = bar.candyBarIconFrameBackdrop
+
+			if iconFrame.IsAnchoringSecret and iconFrame:IsAnchoringSecret() then
+				iconFrame:SetToDefaults()
+				iconBd:SetToDefaults()
+				iconBd:SetFrameLevel(0)
+				reApplyIcon = true
+			end
+
+			statusbar:ClearAllPoints()
+			iconFrame:ClearAllPoints()
+			iconBd:ClearAllPoints()
+
+			if bar:GetIconPosition() == "RIGHT" then
+				statusbar:Point("TOPRIGHT", bar, "TOPRIGHT", 0, 0)
+				statusbar:Point("BOTTOMLEFT", bar, "BOTTOMLEFT", 0, 0)
+
+				iconFrame:Point("TOPLEFT", statusbar, "TOPRIGHT", 4, 0)
+				iconFrame:Point("BOTTOMLEFT", statusbar, "BOTTOMRIGHT", 4, 0)
+			else
+				statusbar:Point("TOPLEFT", bar, "TOPLEFT", 0, 0)
+				statusbar:Point("BOTTOMRIGHT", bar, "BOTTOMRIGHT", 0, 0)
+
+				iconFrame:Point("TOPRIGHT", statusbar, "TOPLEFT", -4, 0)
+				iconFrame:Point("BOTTOMRIGHT", statusbar, "BOTTOMLEFT", -4, 0)
+			end
+
+			iconFrame:SetSize(bar:GetHeight(), bar:GetHeight())
+
+			bar:Set("bigwigs:restoreicon", true)
+			iconBd:SetTemplate("Transparent")
+			iconBd:SetOutside(iconFrame)
+
+			if not E.PixelMode and iconBd.iborder then
+				iconBd.iborder:Show()
+				iconBd.oborder:Show()
+			end
+
+			if shadowsEnabled then
+				iconBd:CreateSoftShadow()
+			end
+
+			iconBd:Show()
+
+			if reApplyIcon then
+				iconFrame:SetTexture(iconTexture)
+				iconFrame:SetTexCoord(0.07, 0.93, 0.07, 0.93)
+			end
+		end
+	end
+
+	_G.BigWigsAPI:RegisterBarStyle(BUI.Title, {
+		apiVersion = 1,
+		version = 10,
+		barSpacing = (E.PixelMode and 4 or 8) or 4,
+		barHeight = 20,
+		spellIndicatorsOffset = 30,
+		ApplyStyle = styleBar,
+		BarStopped = removeStyle,
+		GetStyleName = function() return BUI.Title end,
+	})
+end
+S:AddCallbackForAddon("BigWigs_Plugins", "BenikUI_BigWigs", mod.BigWigs)
