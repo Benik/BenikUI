25ae8ff8d04dbbf4cefc3ab5098b12feb4a0527a
Benik <benik.mobile@gmail.com>
2026-05-31 10:17:32 +0300

    Immersion Skin: Added back the default blue "Hilite" appearance, fixed button spacing when shadows enabled and added some guard flags

--- ElvUI_BenikUI/modules/skins/addons/Immersion.lua
+++ ElvUI_BenikUI/modules/skins/addons/Immersion.lua
@@ -1,67 +1,80 @@
 local BUI, E, L, V, P, G = unpack((select(2, ...)))
 local S = E:GetModule('Skins')
 
-local hooksecurefunc = hooksecurefunc
-local pairs = pairs
+local _G = _G
+local next = next
 local ipairs = ipairs
+local tinsert, tsort = table.insert, table.sort
+
+local hooksecurefunc = hooksecurefunc
 
 local function Immersion()
 	if not (BUI:IsAddOnEnabled('Immersion') and E.db.benikui.skins.variousSkins.immersion) then return end
 
-	local frame = _G.ImmersionFrame.TalkBox
-	if not frame then return end
+	local ImmersionFrame = _G.ImmersionFrame
+	if not ImmersionFrame then return end
 
 	local hasShadows = E.db.benikui.general.benikuiStyle and E.db.benikui.general.shadows
+	local Talkbox = ImmersionFrame.TalkBox
 
-	S:HandleFrame(frame.BackgroundFrame)
-	frame.BackgroundFrame:BuiStyle("Inside")
-	frame.Hilite:Hide()
-	S:HandleFrame(frame.Elements)
+	S:HandleFrame(Talkbox.BackgroundFrame)
+	Talkbox.BackgroundFrame:BuiStyle("Inside")
+	S:HandleFrame(Talkbox.Elements)
 
 	if hasShadows then
-		frame.Elements:CreateSoftShadow()
+		Talkbox.Elements:CreateSoftShadow()
 	end
 
-	S:HandleCloseButton(frame.MainFrame.CloseButton)
+	S:HandleCloseButton(Talkbox.MainFrame.CloseButton)
 
-	frame.PortraitFrame:StripTextures()
+	Talkbox.PortraitFrame:StripTextures()
 
-	local area = {"TopEdge", "Center", "BottomEdge", "BottomLeftCorner", "BottomRightCorner", "LeftEdge", "RightEdge", "TopLeftCorner", "TopRightCorner", "HighlightTexture", "Overlay", "Hilite"}
-	hooksecurefunc(_G.ImmersionFrame.TitleButtons, "UpdateActive", function(self)
-		for _, button in pairs(self.Buttons) do
-			if button.isSkinned then return end
-
-			if button:IsShown() then
+	local area = {"TopEdge", "Center", "BottomEdge", "BottomLeftCorner", "BottomRightCorner", "LeftEdge", "RightEdge", "TopLeftCorner", "TopRightCorner", "HighlightTexture", "Overlay"}
+	hooksecurefunc(ImmersionFrame.TitleButtons, "UpdateActive", function(self)
+		for _, button in next, self.Buttons do
+			if button:IsShown() and not button.isSkinned then
 				for _, region in ipairs(area) do
-					button[region]:Hide()
+					if button[region] then
+						button[region]:Hide()
+					end
 				end
-				S:HandleButton(button)
-				button:CreateBackdrop("Transparent")
-				button.isSkinned = true
+
+				if not button.backdrop then
+					button:CreateBackdrop("Transparent")
+				end
 			end
 		end
 
 		if not hasShadows then return end
 
-		local spacing = 6
+		local spacing = 4
 		local lastButton = nil
+		local activeButtons = {}
 
-		for i, button in pairs(self.Buttons) do
+		for i, button in next, self.Buttons do
+			if button and button:IsShown() then
+				tinsert(activeButtons, { index = i, btn = button })
+			end
+		end
+		tsort(activeButtons, function(a, b) return a.index < b.index end)
+
+		for _, entry in ipairs(activeButtons) do
+			local button = entry.btn
+
 			button.backdrop:CreateSoftShadow()
 			button:ClearAllPoints()
 
-			if i == 1 then
-				button:Point("TOPLEFT", self, "TOPLEFT", 0, 0)
-				button:Point("TOPRIGHT", self, "TOPRIGHT", 0, 0)
+			if not lastButton then
+				button:Point("TOP", 0, 0)
 			else
-				button:Point("TOPLEFT", lastButton, "BOTTOMLEFT", 0, -spacing)
-				button:Point("TOPRIGHT", lastButton, "BOTTOMRIGHT", 0, -spacing)
+				button:Point("TOP", lastButton, "BOTTOM", 0, -spacing)
 			end
+
 			lastButton = button
 		end
-    end)
+	end)
 
-	local pool = _G.ImmersionFrame.Inspector.tooltipFramePool
+	local pool = ImmersionFrame.Inspector.tooltipFramePool
 	local original_Acquire = pool.Acquire
 
 	pool.Acquire = function(self, ...)
